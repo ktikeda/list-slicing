370aa5eb57ab0b6f69cd04ace1f3040238efb183
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-05 12:33:44 -0700

    Added del() to delete_third_and_seventh()

--- list_operations.py
+++ list_operations.py
@@ -203,8 +203,9 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    input_list[2:3], input_list[5:6] = [], []
-
+    #input_list[2:3], input_list[5:6] = [], []
+    del(input_list[2])
+    del(input_list[5])
 
 
 def delete_middle(input_list):
